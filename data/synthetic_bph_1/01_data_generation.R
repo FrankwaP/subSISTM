faed0220e32b7feff6721936d2286ab8c9f82c35
FrankwaP <francois.plessier@gmail.com>
2024-09-18 10:14:28 +0200

    synth data with only fixed effects

--- data/synthetic_bph_1/01_data_generation.R
+++ data/synthetic_bph_1/01_data_generation.R
@@ -6,7 +6,7 @@ set.seed(13092001)
 
 ind <- 1:500
 time <- 0:50
-l = list(ind, time)
+l <- list(ind, time)
 dataframe <- rev(expand.grid(rev(l)))
 colnames(dataframe) <- c("individus", "temps")
 k <- 8
@@ -24,7 +24,8 @@ mgamma <- numeric(3)
 sdgamma <- diag(c(0.5, 0.5, 0.05))
 g <- mvrnorm(length(ind), mgamma, sdgamma)
 
-simul <- function(individus = ind, k = 5, df = dataframe, µ0 = mu0, µ1 = mu1, sigma0 = sig0,
+simul <- function(
+    individus = ind, k = 5, df = dataframe, µ0 = mu0, µ1 = mu1, sigma0 = sig0,
     sigma1 = sig1, gamma = g, sigeps = sigma_epsilon) {
     alpha0 <- mvrnorm(length(individus), µ0, sigma0)
     alpha1 <- mvrnorm(length(individus), µ1, sigma1)
@@ -37,20 +38,31 @@ simul <- function(individus = ind, k = 5, df = dataframe, µ0 = mu0, µ1 = mu1, 
     df$x3_obs <- df$x3 + rnorm(length(df$x3), 0, sigeps[3])
     df$x4 <- alpha0[df$individus, 4] + alpha1[df$individus, 4] * exp(-0.1 * df$temps)
     df$x4_obs <- df$x4 + rnorm(length(df$x4), 0, sigeps[4])
-    df$x5 <- alpha0[df$individus, 5]/(1 + exp(-alpha1[df$individus, 5] * df$temps))
+    df$x5 <- alpha0[df$individus, 5] / (1 + exp(-alpha1[df$individus, 5] * df$temps))
     df$x5_obs <- df$x5 + rnorm(length(df$x5), 0, sigeps[5])
     df$x6 <- pmax(0, (alpha0[df$individus, 6] + 0.1 * alpha1[df$individus, 6] * df$temps)^2)
     df$x6_obs <- df$x6 + rnorm(length(df$x6), 0, sigeps[6])
-    df$x7 <- alpha0[df$individus, 7]/(1 + exp(alpha1[df$individus, 7] * df$temps))
+    df$x7 <- alpha0[df$individus, 7] / (1 + exp(alpha1[df$individus, 7] * df$temps))
     df$x7_obs <- df$x7 + rnorm(length(df$x7), 0, sigeps[7])
     df <- df %>%
-        mutate(x8 = ifelse(df$individus%%2 == 0, 1, 0))
+        mutate(x8 = ifelse(df$individus %% 2 == 0, 1, 0))
 
-    df$y <- gamma[df$individus, 1] + gamma[df$individus, 2] * df$x1 * df$x5 + gamma[df$individus,
-        3] * df$x2 * df$x6
-    df$y_obs <- df$y + rnorm(length(df$y), 0, sigeps[8])
+    df$y_mixed <- gamma[df$individus, 1] +
+        gamma[df$individus, 2] * df$x1 * df$x5 +
+        gamma[df$individus, 3] * df$x2 * df$x6
+
+    df$y_mixed_obs <- df$y_mixed + rnorm(length(df$y_mixed), 0, sigeps[8])
+
+
+    df$y_fixed <- gamma[1, 1] +
+        gamma[1, 2] * df$x1 * df$x5 +
+        gamma[1, 3] * df$x2 * df$x6
+
+
+    df$y_fixed_obs <- df$y_fixed + rnorm(length(df$y_fixed), 0, sigeps[8])
+
     return(df)
 }
 dataframe <- simul()
-# plot(time, dataframe[dataframe$individus == 4, ]$y_obs, type = "l")
-write.csv2(x=dataframe, file=paste(this.dir(), "/simulation.csv", sep = ""), row.names=FALSE)
+
+write.csv2(x = dataframe, file = paste(this.dir(), "/simulation.csv", sep = ""), row.names = FALSE)
